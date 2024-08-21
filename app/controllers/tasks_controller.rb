d045d325d92cf8aa5ae430be3d5cb27c132c4ede
madhureshb <bhagwat.madhuresh97@gmail.com>
2024-08-21 19:27:35 +0530

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "Task is created successfully"
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:notice] = "Task updated successfully"
+      redirect_to task_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    flash[:notice] = "Task is deleted succesfully"
+    redirect_to root_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
+end
